c69548092303a6cee6a5157fbc48473fb53f3329
Aprilia53 <Aprilia.galata@ucas-edu.net>
2024-10-22 20:42:25 +0000

    made part of aprilias code

--- final_project.py
+++ final_project.py
@@ -1,6 +1,13 @@
 #setting up story for game and ask a name - everyone
+print("Welcome to our game! What is your name?")
+name = input()
+print("Hello," +name+"! you are on a mission to climb the tallest mountain in your small town! first youll need supplise.")
 
 #get a map and supplies, in village (ask what they want to buy first) - Aprilia (function)
+print("You walk to your nearby shops, and your overwhelmed with how many supplies you need.")
+print("The shop owner says: Are you"+name+"? I hear you are climbing to the tallest mountain in the city!")
+print("You'll need lots of supplies. Starting with a rope, water bottle, shoes, food, warmth, and a tent. ")
+print
 
 #ask user where they would like to go next  - (stop and eat, go into cave, keep going) Lola  (conditional)
 
