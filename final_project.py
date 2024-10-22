4246bfebe6528ecd55e2c084f5db3cef04fb49c8
Aprilia53 <Aprilia.galata@ucas-edu.net>
2024-10-22 19:45:33 +0000

    made stuff
    Co-authored-by: franco-barboza27 <franco-barboza27@users.noreply.github.com>
    Co-authored-by: Lolawatson1 <Lolawatson1@users.noreply.github.com>

---
+++ final_project.py
@@ -0,0 +1,11 @@
+#setting up story for game and ask a name - everyone
+
+#get a map and supplies, in village (ask what they want to buy first) - Aprilia (function)
+
+#ask user where they would like to go next  - (stop and eat, go into cave, keep going) Lola  (conditional)
+
+#User runs in with yeti (yeti gives options to get food, )helte- or a (better) map  Franco (function)
+
+#user makes it to the top and plants flag (it you wanna stay, eat something, plant a flag) - Tara (loop)
+
+#User goes back down (deciding wheather they want to live there, go back down, or go on another adventure)- (everyone)
